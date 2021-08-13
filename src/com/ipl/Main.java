dd210ebb8e63dcf4c505d0f4667752624465ff58
SwagataBhol <swagata.bhol.16@mountblue.tech>
2021-08-13 11:09:23 +0530

    solved 5th problem

--- src/com/ipl/Main.java
+++ src/com/ipl/Main.java
@@ -77,6 +77,7 @@ static List<Match> readMatchesData() {
 				  match.setSeason(data[season]);
 				  match.setWinner(data[winner]);
 				  match.setId(data[id]);
+				  match.setTossWinner(data[tossWinner]);
 				 matches.add(match);
 				 
 			}
@@ -299,13 +300,39 @@ static HashMap<String,Float> TopTenEconomicalBowlers2015(List<Deliveries> delive
 		if(topTenEcoBowler.containsKey(sublist.get(i))) {
 			
 			String BowlerName=topTenEcoBowler.get(sublist.get(i));
-			topTenEcoBowler2015.put(BowlerName, sublist.get(i));
+			
+			topTenEcoBowler2015.put(BowlerName,sublist.get(i));
 
 		}
 	}
-	System.out.println(topTenEcoBowler2015);
+//	System.out.println(topTenEcoBowler2015);
 	return topTenEcoBowler2015;
 	
+}
+
+static HashMap<String,Integer> TossAndMatchesWonPerTeam(List<Match> matchData){
+	
+	HashMap<String,Integer> wonTossAndMatch=new HashMap<String,Integer>();
+	for(int i=0;i<matchData.size();i++) {
+		
+		String wonToss=matchData.get(i).getTossWinner();
+		String winner=matchData.get(i).getWinner();
+		
+		if(wonToss.equals(winner)) {
+			
+			if(wonTossAndMatch.containsKey(winner)) {
+				
+				int c=wonTossAndMatch.get(winner);
+				c+=1;
+				wonTossAndMatch.put(winner, c);
+			}
+			else {
+				
+				wonTossAndMatch.put(winner, 1);
+			}
+		}
+	}
+	return wonTossAndMatch;
 }
 
 
@@ -330,154 +357,16 @@ static HashMap<String,Float> TopTenEconomicalBowlers2015(List<Deliveries> delive
 	problem3.forEach((k,v) -> System.out.println("Key = "
 			+ k + ", Value = " + v));
 	
-	HashMap<String,Float> problem4=TopTenEconomicalBowlers2015(deliveryyData,matchData);
-//	problem4.forEach((k,v) -> System.out.println("Key = "
-//			+ k + ", Value = " + v));
+	HashMap<String,Float> problem4=TopTenEconomicalBowlers2015(deliveryyData,matchData);//top 10 economical bowler in 2015
+	problem4.forEach((k,v) -> System.out.printf("Key = "
+			+ k + ", Value = " +"%.02f", v));
+	
+	HashMap<String,Integer> problem5=TossAndMatchesWonPerTeam(matchData);// Find the number of times each team won the toss and also won the match
+	problem5.forEach((k,v) -> System.out.printf(" Key = "
+			+ k + ", Value = " + v));
 	}
 }
 	
 //######################################################################################################################
 //####################################################################################################################	
-// previous data
-	
-//	String keys[]= {"id","season","city","date","team1","team2","toss_winner","toss_decision","result","dl_applied","winner","win_by_runs","win_by_wickets","player_of_match","venue","umpire1","umpire2","umpire3"};
-	
-	
-//	String path1="./data/matches.csv";
-//	String path2="./data/deliveries.csv";
-//	String line="";
-//	String split=",";
-//	
-//	
-//	ArrayList<String[]> match = new ArrayList<String[]>();
-//	HashMap<String, Integer> matchData1 = new HashMap<String,Integer>();
-//	HashMap<String, HashMap<String, Integer>> matchData2 = new HashMap<String, HashMap<String, Integer>>();
-//	
-//	ArrayList<String[]> deliveties=new ArrayList<String[]>();
-//	HashMap<String, Integer> deliveryData = new HashMap<String,Integer>();
-////	String[] Ids16={};
-//	List<String> Ids16=new ArrayList<String>();
-//	
-//	
-//	
-//	int skip=0;
-//	int count1=0;
-//	int count2=0;
-//	
-//	try {
-//		BufferedReader bf=new BufferedReader(new FileReader(path1));
-//		while((line=bf.readLine())!= null) 
-//		{
-//			  if(skip == 0){
-//                  skip++;
-//                  continue;
-//              }
-//			  String[] data=line.split(split);
-//			  
-//			 match.add(data);
-//			 
-////			 matchesPlayedPerYear
-//			 if(matchData1.containsKey(data[1])==true) {
-////				 count1+=1;
-//				 int cnt=matchData1.get(data[1]);
-//				 cnt+=1;
-//				 matchData1.put(data[1], cnt);
-//			 }
-//			 else {
-//				 
-//				 count1=1;
-//				 matchData1.put(data[1], count1);
-//			 }
-//			 
-////			 matches won per team per year 
-//		 if(matchData2.containsKey(data[1])==true) {
-//			 if(matchData2.get(data[1]).containsKey(data[10])==true) {
-//
-//				 int c=matchData2.get(data[1]).get(data[10]);
-//				 c+=1;
-//				 matchData2.get(data[1]).put(data[10],c);
-//
-//					 
-//
-//				 }
-//				 else {
-//					 count2=1;
-//					 matchData2.get(data[1]).put(data[10],count2);
-//				 }
-//				 
-//			 }
-//			 else {
-//				 count2=1;
-//				 HashMap<String, Integer> winner = new HashMap<String,Integer>();
-//				 winner.put(data[10],count2);
-//				 matchData2.put(data[1],winner );
-//			 }
-////		filtering data for 2016 
-//		 if(matchData2.containsKey("2016")){
-//			 Ids16.add(data[0]);
-//			 
-//		 }
-////		 end of while loop
-//		}
-//
-////		deliveries data read
-//		 int skip1=0;
-//		 BufferedReader br=new BufferedReader(new FileReader(path2));
-//			while((line=br.readLine())!= null) 
-//			{
-//				  if(skip1 == 0){
-//	                  skip1++;
-//	                  continue;
-//	              }
-//				  String[] data1=line.split(split);
-//				  
-//				  deliveties.add(data1);
-//				  
-//		}
-////			System.out.println(deliveties.get(0)[16]); (how to get values of ArrayList)
-//		
-//	
-//		bf.close();
-//	}
-//	
-//	catch(IOException e) {
-//		e.printStackTrace();
-//	}
-////	matchData1.forEach((k,v) -> System.out.println("Key = "
-////           + k + ", Value = " + v));
-////	matchData2.forEach((k,v) -> System.out.println("Key = "
-////           + k + ", Value = " + v));
-////    
-//
-////	Extra runs conceded per team in the year 2016
-//for(int i=0;i<deliveties.size();i++) {
-//	String id=deliveties.get(i)[0];
-//	
-//	
-//	if(Ids16.contains(id)) {
-//		
-//		String bowlingTeam=deliveties.get(i)[3];
-//		String runs=deliveties.get(i)[16];
-//		int extraRuns=Integer.parseInt(runs);
-//		
-//		if(deliveryData.containsKey(bowlingTeam)) {
-//			
-//			int cnt=deliveryData.get(bowlingTeam);
-//			cnt+=extraRuns;
-//			deliveryData.put(bowlingTeam,cnt);
-//		}
-//		else {
-//			
-//			deliveryData.put(bowlingTeam,extraRuns );
-//		}
-//		
-//	}
-//	
-//}
-////deliveryData.forEach((k,v) -> System.out.println("Key = "
-////   + k + ", Value = " + v));
-//
-//}
-//}
-//
 
